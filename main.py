971c11cf24c58a2fcba851999484a7000e6c205b
Evelyn <evelyn.ciara.345@gmail.com>
2025-09-17 20:15:37 -0400

    start writing sqlite3 logic

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import argparse
+import sqlite3
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    return parser.parse_args()
+
 def main():
-    pass
+    args = get_args()
+    filename = args.filename
+
+    # .aup3 is an SQLite3 database
+    conn = sqlite3.connect(filename)
+    cursor = conn.cursor()
 
 
 if __name__ == "__main__":
