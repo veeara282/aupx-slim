6bcb0d18fbb8fe843a1c12fa22967f76fc99d18a
Evelyn <evelyn.ciara.345@gmail.com>
2025-09-17 20:27:08 -0400

    Wrap sqlite3 ops in object

--- main.py
+++ main.py
@@ -2,6 +2,29 @@ import argparse
 import sqlite3
 
 
+# Sample format IDs as defined in Audacity's SampleFormat.h
+undefinedSample = 0
+int16Sample = 0x00020001
+int24Sample = 0x00040001
+floatSample = 0x0004000F
+
+
+class AUP3ProjectWrapper:
+    def __init__(self, filename):
+        # .aup3 is an SQLite3 database
+        self.conn = sqlite3.connect(filename)
+        self.cursor = self.conn.cursor()
+    
+    def __del__(self):
+        self.conn.close()
+    
+    def list_silent_blocks(self):
+        self.cursor.execute(
+            "select * from sampleblocks where summin = 0 and summax = 0"
+            )
+        rows = self.cursor.fetchall()
+        return rows
+
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("filename")
@@ -11,9 +34,7 @@ def main():
     args = get_args()
     filename = args.filename
 
-    # .aup3 is an SQLite3 database
-    conn = sqlite3.connect(filename)
-    cursor = conn.cursor()
+    project = AUP3ProjectWrapper(filename)
 
 
 if __name__ == "__main__":
